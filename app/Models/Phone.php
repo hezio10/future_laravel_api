327adc07086dc1a0a5bd135879a7c24ce0014de7
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2025-04-12 17:22:25 +0200

    feat: add relationship between Phone and PhoneLabel models

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -5,8 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\PhoneLabel;
+
 class Phone extends Model
 {
     /** @use HasFactory<\Database\Factories\PhoneFactory> */
     use HasFactory;
+
+    public function label()
+    {
+        return $this->belongsTo(PhoneLabel::class, 'label_id');
+    }
 }
--- app/Models/PhoneLabel.php
+++ app/Models/PhoneLabel.php
@@ -5,8 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Phone;
+
 class PhoneLabel extends Model
 {
     /** @use HasFactory<\Database\Factories\PhoneLabelsFactory> */
     use HasFactory;
+
+    public function phone()
+    {
+        return $this->hasMany(Phone::class, 'label_id');
+    }
 }
