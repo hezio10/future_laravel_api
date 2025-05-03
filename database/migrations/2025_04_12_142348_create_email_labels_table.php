b503f36a07cc97d01667b7d95c082571db9f401f
Hezio Cumba <heziocumba@gmail.com>
2025-05-03 16:46:16 +0200

    ref: add name column to create_email_labels_table migration

--- database/migrations/2025_04_12_142348_create_email_labels_table.php
+++ database/migrations/2025_04_12_142348_create_email_labels_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('email_labels', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->datetimes();
             $table->charset("utf8mb4");
             $table->collation("utf8mb4_0900_ai_ci");
