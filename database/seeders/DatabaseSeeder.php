200f1bc407c4038eccd0e10acc7ef44fe6dc17a8
Hezio Cumba <heziocumba@gmail.com>
2025-05-11 15:42:04 +0200

    feat: implement initial database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\Contact;
+use App\Models\Email;
+use App\Models\EmailLabel;
+use App\Models\Phone;
+use App\Models\PhoneLabel;
+use App\Models\Tag;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +20,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        PhoneLabel::factory(5)->create();
+        EmailLabel::factory(5)->create();
+        Contact::factory(5)->create();
+        Tag::factory(5)->create();
+
+       DB::table('contact_tags')->insert(
+            [
+                'contact_id' => 1,
+                'tag_id' => 1,
+            ],
+            
+        );
+        DB::table('contact_tags')->insert(
+            [
+                'contact_id' => 1,
+                'tag_id' => 3,
+            ],
+        );
+        DB::table('contact_tags')->insert(
+            [
+                'contact_id' => 3,
+                'tag_id' => 4,
+            ],
+        );
+        DB::table('contact_tags')->insert(
+           [
+                'contact_id' => 4,
+                'tag_id' => 4,
+            ]
+        );
+        
+        Email::factory(15)->create();
+        Phone::factory(15)->create();
+
+
     }
 }
