2ea7b9bfc7d54059719084a1cb62c0dcf1b1f9a2
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:08:13 +0200

    add return type hint to phones method in PhoneLabel model

--- app/Models/PhoneLabel.php
+++ app/Models/PhoneLabel.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Phone;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PhoneLabel extends Model
 {
     /** @use HasFactory<\Database\Factories\PhoneLabelsFactory> */
     use HasFactory;
 
-    public function phones()
+    public function phones(): HasMany
     {
         return $this->hasMany(Phone::class, 'label_id');
     }
