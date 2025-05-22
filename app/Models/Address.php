6ee0c67209082390d3dba8a88eab5376f0e62ee0
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:06:37 +0200

    ref: add return type hint to contact method in Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Contact;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Address extends Model
 {
     /** @use HasFactory<\Database\Factories\AddressFactory> */
     use HasFactory;
 
-    public function contact() {
+    public function contact(): HasOne
+    {
         return $this->hasOne(Contact::class);
     }
 }
