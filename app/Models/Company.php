0dae46d8b94f17589261828f3076525ad3f4c858
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 22:54:18 +0200

    ref: update docblock for Company model to enhance property descriptions

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,11 +3,21 @@
 namespace App\Models;
 
 use App\Models\Contact;
-
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * Representa uma empresa associada a um contacto.
+ * 
+ * @property int $id id da empresa.
+ * @property string $name nome da empresa.
+ * @property string $role cargo do colaborador na empresa.
+ * @property Contact|null $contact contato associado a empresa.
+ * @property Carbon|null $created_at data de criação do registro.
+ * @property Carbon|null $updated_at data de atualização do registro.
+ */
 class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
