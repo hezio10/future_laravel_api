4d592816823056123283330b9ee036789f9d7178
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:06:45 +0200

    ref: add return type hint to contact method in Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,13 +6,15 @@ use App\Models\Contact;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
 
-    public function contact() {
+    public function contact(): HasOne
+    {
         return $this->hasOne(Contact::class);
     }
 }
