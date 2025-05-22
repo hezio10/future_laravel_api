661b216b9b59bf90e0cf8da80879b25f560bca0b
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:07:51 +0200

    ref: add return type hint to emails method in EmailLabel model

--- app/Models/EmailLabel.php
+++ app/Models/EmailLabel.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class EmailLabel extends Model
 {
     /** @use HasFactory<\Database\Factories\EmailLabelFactory> */
     use HasFactory;
 
-     public function emails()
+     public function emails(): HasMany
     {
         return $this->hasMany(Phone::class, 'label_id');
     }
