bc92cd4464c7f1dfd8a6e7f2fa5bb67eedddb70d
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:07:41 +0200

    add return type hints to label and contact methods in Email model

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -7,18 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Models\EmailLabel;
 use App\Models\Contact;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Email extends Model
 {
     /** @use HasFactory<\Database\Factories\EmailFactory> */
     use HasFactory;
 
-    public function label()
+    public function label(): BelongsTo
     {
         return $this->belongsTo(EmailLabel::class, 'label_id');
     }
 
-    public function contact()
+    public function contact(): BelongsTo
     {
         return $this->belongsTo(Contact::class);
     }
