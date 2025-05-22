241216abd6df46759659b66ff5ad11b07b9a9262
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:03:43 +0200

    ref: add return type hints to relationship methods in Contact model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -9,33 +9,36 @@ use App\Models\Tag;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
     /** @use HasFactory<\Database\Factories\ContactFactory> */
     use HasFactory;
 
-    public function address()
+    public function address(): BelongsTo
     {
         return $this->belongsTo(Address::class);
     }
 
-    public function company()
+    public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
     }
 
-    public function phones()
+    public function phones(): HasMany
     {
         return $this->HasMany(Phone::class);
     }
 
-    public function emails()
+    public function emails(): HasMany
     {
         return $this->HasMany(Email::class);
     }
 
-    public function tags() 
+    public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, 'contact_tags');
     }
