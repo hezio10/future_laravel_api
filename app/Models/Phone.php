5c66929ce26888458c7031911bb2c4e0d5b9694c
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:08:05 +0200

    add return type hints to label and contact methods in Phone model

--- app/Models/Phone.php
+++ app/Models/Phone.php
@@ -7,18 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Models\PhoneLabel;
 use App\Models\Contact;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Phone extends Model
 {
     /** @use HasFactory<\Database\Factories\PhoneFactory> */
     use HasFactory;
 
-    public function label()
+    public function label(): BelongsTo
     {
         return $this->belongsTo(PhoneLabel::class, 'label_id');
     }
 
-    public function contact()
+    public function contact(): BelongsTo
     {
         return $this->belongsTo(Contact::class);
     }
