4021beac3b99177da8e9260a598e8abfa46ce77b
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 19:08:24 +0200

    ref: add return type hint to contacts method in Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Contact;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
 
-    public function contacts()
+    public function contacts(): BelongsToMany
     {
         return $this->belongsToMany(Contact::class, 'contact_tags');
     }
