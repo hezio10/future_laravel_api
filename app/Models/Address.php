0181f677b2c3ba7e6b94f0e6d6d435b8aa13000f
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 22:54:08 +0200

    ref: update docblock for Address model to enhance property descriptions

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,8 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Contact;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * Representa um endereço associado a um contato.
+ *
+ * @package App\Models
+ *
+ * @property int $id identificador único do endereço.
+ * @property string $street nome da rua ou avenida.
+ * @property string $neighborhood nome do bairro.
+ * @property Contact $contact Contato associado ao endereço.
+ * @property Carbon|null $created_at data de criação do registro.
+ * @property Carbon|null $updated_at data de atualização do registro.
+ */
 class Address extends Model
 {
     /** @use HasFactory<\Database\Factories\AddressFactory> */
