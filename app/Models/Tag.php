67db7ca505e18e94d2f40626cd9656dc4a071413
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-23 20:24:04 +0200

    docs: update docblock for Tag model to improve clarity and property descriptions

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Contact;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * Representa uma etiqueta usada para classificar os contactos(Ex: trabalho, familia, amigo, outro.).
+ * 
+ * @property int $id identificador unico.
+ * @property string $name Nome da etiqueta (ex: pessoal, trabalho).
+ *
+ * @property Collection|Email[] $contacts Lista de contactos com essa etiqueta.
+ */
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
