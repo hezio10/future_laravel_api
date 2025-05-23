bc070ab0f9d2f0c626295b455de071dabe165b8f
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-23 20:20:47 +0200

    docs: update docblock for PhoneLabel model to improve clarity and property descriptions

--- app/Models/PhoneLabel.php
+++ app/Models/PhoneLabel.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Phone;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * Representa uma etiqueta usada para classificar os tipos de telefone(Ex: trabalho, familia, amigo, outro.).
+ * 
+ * @property int $id identificador unico.
+ * @property string $name Nome da etiqueta (ex: pessoal, trabalho).
+ *
+ * @property Collection|Email[] $phones Lista de telefones com essa etiqueta.
+ */
 class PhoneLabel extends Model
 {
     /** @use HasFactory<\Database\Factories\PhoneLabelsFactory> */
