9a731a90e2fd571a2b92de3a7a8a3d840397ef01
Hezio Cumba <heziocumba@gmail.com>
2025-05-31 17:58:27 +0200

    feat: implement update, store, and index routes

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use App\Http\Controllers\V1\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/contacts', [ ContactController::class, 'index']);
+    Route::prefix('contacts')->group(function () {
+        Route::get('', [ ContactController::class, 'index']);
+        Route::post('', [ ContactController::class, 'store']);
+        Route::get('/{id}', [ ContactController::class, 'show']);
+        Route::put('/{id}', [ ContactController::class, 'update']);
+    });
 });
