95f264229259a0347a0db56bfb33b2d6afbff870
Hezio Cumba <heziocumba@gmail.com>
2026-02-07 18:31:09 +0200

    change: rename signup endpoint to sign-up

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
-        Route::post('/signup', [AuthController::class, 'signUp']);
+        Route::post('/sign-up', [AuthController::class, 'signUp']);
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/logout', [AuthController::class, 'logout'])->middleware(AuthMiddleware::class);
     });
