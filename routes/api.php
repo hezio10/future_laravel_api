a3ebf241f0e14e872c573b93c3ded6c1484dbae6
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-02-11 23:09:52 +0200

    feat: add auth middleware on refresh-token route

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::prefix('v1')->group(function () {
         Route::post('/sign-up', [AuthController::class, 'signUp']);
         Route::post('/sign-in', [AuthController::class, 'signIn']);
         Route::post('/sign-out', [AuthController::class, 'signOut'])->middleware(AuthMiddleware::class);
-        Route::post('/refresh-token', [AuthController::class, 'refreshToken']);
+        Route::post('/refresh-token', [AuthController::class, 'refreshToken'])->middleware(AuthMiddleware::class);
     });
 
     Route::prefix('contacts')->group(function () {
